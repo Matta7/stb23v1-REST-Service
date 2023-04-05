1a01ae4d643e05db0550fb850ab0b7e65916a326
Mathieu Vallee <mathieu.vallee7@gmail.com>
2023-04-05 14:29:19 +0200

    First commit

---
+++ src/main/java/fr/univrouen/stb23v1/Stb23v1Application.java
@@ -0,0 +1,15 @@
+package fr.univrouen.stb23v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stb23v1Application {
+
+	public static void main(String[] args) {
+		// http://localhost:8100/
+		System.getProperties().put("server.port", 8100);
+		SpringApplication.run(Stb23v1Application.class, args);
+	}
+
+}
