7b7fd9255f21e7c05b277242c129ce4ba4d1d8bf
Matta <104003130+Matta7@users.noreply.github.com>
2023-05-25 14:13:21 +0200

    Test deploy

--- client/src/main/java/fr/univrouen/stb23v1/model/services/HomeService.java
+++ client/src/main/java/fr/univrouen/stb23v1/model/services/HomeService.java
@@ -1,13 +1,28 @@
 package fr.univrouen.stb23v1.model.services;
 
+import fr.univrouen.stb23v1.model.api.HomeRequest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class HomeService {
 
-    final String uri = "http://localhost:8080";
+    HomeRequest homeRequest = new HomeRequest();
 
     public String getHomePage() {
-        return "";
+
+        String result = homeRequest.homePageRequest();
+
+        Document document = Jsoup.parse(result.split("<!DOCTYPE html>")[1]);
+        System.out.println(document.body().text());
+
+        return homeRequest.homePageRequest();
+    }
+
+    public String getHelpPage() {
+        return homeRequest.helpPageRequest();
     }
 }
