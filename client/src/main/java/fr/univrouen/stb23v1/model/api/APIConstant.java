e5be294453d478737d0556970520323c9bf78876
Matta <104003130+Matta7@users.noreply.github.com>
2023-05-27 01:53:36 +0200

    Fixed application.properties file

--- client/src/main/java/fr/univrouen/stb23v1/model/api/APIConstant.java
+++ client/src/main/java/fr/univrouen/stb23v1/model/api/APIConstant.java
@@ -2,6 +2,6 @@ package fr.univrouen.stb23v1.model.api;
 
 public class APIConstant {
     //public static String baseURL = "http://localhost:8080";
-    public static String baseURL = "https://stb23-merre-vallee-rest-service.cleverapps.io/";
+    public static String baseURL = "https://stb23-merre-vallee-rest-service.cleverapps.io";
 
 }
